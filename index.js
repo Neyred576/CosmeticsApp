3e7df3a29d030a8710c79f897dc4616914291202
Prosper <prospermwendwa2006@gmail.com>
2026-02-26 18:33:30 +0300

    Fix: Add polyfill for accessibilityContainerViewIsModal prop error in RN 0.78

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+// Workaround for React Native 0.78.0 codegen bug
+if (!View.prototype.accessibilityContainerViewIsModal) {
+    View.prototype.accessibilityContainerViewIsModal = false;
+}
 
 AppRegistry.registerComponent(appName, () => App);
