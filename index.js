33787f641f2cfe8f7010b90352523c87479d7e0b
Prosper <prospermwendwa2006@gmail.com>
2026-02-26 18:40:36 +0300

    Fix: Downgrade to highly stable RN 0.76.5 and Screens v3 to resolve codegen errors

--- index.js
+++ index.js
@@ -2,13 +2,8 @@
  * @format
  */
 
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-
-// Workaround for React Native 0.78.0 codegen bug
-if (!View.prototype.accessibilityContainerViewIsModal) {
-    View.prototype.accessibilityContainerViewIsModal = false;
-}
 
 AppRegistry.registerComponent(appName, () => App);
